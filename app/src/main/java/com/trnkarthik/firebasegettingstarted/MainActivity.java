b8ae676221ba343fbd80af59707123fdf7fefd36
Karthik Tangirala <karthik.trn@gmail.com>
2015-02-24 20:13:02 -0700

    Initial commit
    Initial commit

---
+++ app/src/main/java/com/trnkarthik/firebasegettingstarted/MainActivity.java
@@ -0,0 +1,70 @@
+package com.trnkarthik.firebasegettingstarted;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        Firebase.setAndroidContext(this);
+
+        Firebase myFirebaseRef = new Firebase(getString(R.string.firebase_url));
+
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
+
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                textView.setText(snapshot.getValue().toString());
+            }
+
+            @Override public void onCancelled(FirebaseError error) { }
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
